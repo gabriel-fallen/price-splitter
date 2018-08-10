cb1056113ec4f244e5a893bf5b7f71fd497c0ab7
Alexander Tchitchigin <sad.ronin@gmail.com>
2018-08-10 19:15:14 +0300

    Not mesuring initialization and drop anymore.

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -4,36 +4,84 @@ extern crate rand;
 
 use criterion::Criterion;
 use rand::prelude::*;
+use std::cell::RefCell;
 
 extern crate price_split;
 
 use price_split::splitter::*;
 
 
-fn bench_insert(c: &mut Criterion) {
+fn bench_insert_2000(c: &mut Criterion) {
   let nprices = 100;
+  let npairs  = 20;
   let mut rng = thread_rng();
 
-  c.bench_function_over_inputs("insert", move |b, &&npairs| {
-    let mut queue: Queue = vec![];
-    for _ in 0 .. nprices {
-      let price: Price = rng.gen();
-      for _ in 0 .. npairs {
-        let size: Size = rng.gen();
-        insert(&mut queue, price, size, 1234);
-      }
+  let queue = RefCell::new(Queue::new(vec![]));
+  for _ in 0 .. nprices {
+    let price: Price = rng.gen();
+    for _ in 0 .. npairs {
+      let size: Size = rng.gen();
+      queue.borrow_mut().insert(price, size, 1234);
     }
-    let p = rng.gen(); // FIXME: should I move it into `iter`?
-    let s = rng.gen();
-    b.iter(|| insert(&mut queue, p, s, 432))
-  }, &[20, 70, 200]);
+  }
+
+  c.bench_function("insert 2000", move |b| {
+    b.iter_with_setup(|| {
+      queue.borrow_mut().pop_rand();
+      (rng.gen(), rng.gen())
+    }, |(p, s)| queue.borrow_mut().insert(p, s, 432))
+  });
+}
+
+fn bench_insert_7000(c: &mut Criterion) {
+  let nprices = 100;
+  let npairs  = 70;
+  let mut rng = thread_rng();
+
+  let queue = RefCell::new(Queue::new(vec![]));
+  for _ in 0 .. nprices {
+    let price: Price = rng.gen();
+    for _ in 0 .. npairs {
+      let size: Size = rng.gen();
+      queue.borrow_mut().insert(price, size, 1234);
+    }
+  }
+
+  c.bench_function("insert 7000", move |b| {
+    b.iter_with_setup(|| {
+      queue.borrow_mut().pop_rand();
+      (rng.gen(), rng.gen())
+    }, |(p, s)| queue.borrow_mut().insert(p, s, 432))
+  });
+}
+
+fn bench_insert_20000(c: &mut Criterion) {
+  let nprices = 100;
+  let npairs  = 200;
+  let mut rng = thread_rng();
+
+  let queue = RefCell::new(Queue::new(vec![]));
+  for _ in 0 .. nprices {
+    let price: Price = rng.gen();
+    for _ in 0 .. npairs {
+      let size: Size = rng.gen();
+      queue.borrow_mut().insert(price, size, 1234);
+    }
+  }
+
+  c.bench_function("insert 20000", move |b| {
+    b.iter_with_setup(|| {
+      queue.borrow_mut().pop_rand();
+      (rng.gen(), rng.gen())
+    }, |(p, s)| queue.borrow_mut().insert(p, s, 432))
+  });
 }
 
 fn bench_split_half(c: &mut Criterion) {
-  let mut queue: Queue = vec![];
+  let mut queue      = Queue::new(vec![]);
   let mut total_size = 0;
-  let mut rng = thread_rng();
-  let nprices = rng.gen_range::<u32>(100, 1000);
+  let mut rng        = thread_rng();
+  let nprices: u32   = rng.gen_range(100, 1000);
 
   for _ in 0 .. nprices {
     let price: Price = rng.gen();
@@ -41,44 +89,66 @@ fn bench_split_half(c: &mut Criterion) {
     for _ in 0 .. npairs {
       let size: Size = rng.gen();
       total_size += size;
-      insert(&mut queue, price, size, 1234);
+      queue.insert(price, size, 1234);
     }
   }
 
   let s = total_size / 2;
   c.bench_function("split in halves", move |b| {
-    let mut q = queue.clone();
-    let     p = rng.gen();  // on average this should split prices roughly in equal halfs, like in qsort
-    b.iter(|| split(&mut q, p, s))
+    b.iter_with_setup(|| {
+      let q = queue.clone();
+      let p = rng.gen();  // on average this should split prices roughly in equal halfs, like in qsort
+      (q, p)
+    }, |(mut q, p)| drop(q.split(p, s)))
   });
 }
 
 fn bench_split_n(c: &mut Criterion) {
-  let mut rng = thread_rng();
-  let nprices = 100;
+  let mut rng     = thread_rng();
+  let     nprices = 100;
+  let     npairs  = 70;
+  let     queue   = RefCell::new(Queue::new(vec![]));
+  let mut nelems  = 0;
+
+  for _ in 0 .. nprices {
+    let price: Price = rng.gen();
+    for _ in 0 .. npairs {
+      let size: Size = rng.gen();
+      queue.borrow_mut().insert(price, size, 1234);
+    }
+  }
 
   c.bench_function_over_inputs("split N", move |b, &&n| {
-    let mut queue: Queue = vec![];
-    let mut size_limit   = 0;
-    let mut nelems       = 0;
-    let p = std::i32::MAX;
+    b.iter_with_setup(|| {
+      let mut size_limit = 0;
 
-    for _ in 0 .. nprices {
-      let price: Price = rng.gen();
-      let npairs: u32  = 70;
-      for _ in 0 .. npairs {
+      // restore original number of elements
+      for _ in 0 .. nelems {
+        let price: Price = rng.gen();
         let size: Size = rng.gen();
-        if nelems < n {
-          size_limit += size;
-          nelems += 1;
-        }
-        insert(&mut queue, price, size, 1234);
+        queue.borrow_mut().insert(price, size, 1234);
       }
-    }
 
-    b.iter(|| split(&mut queue, p, size_limit))
+      nelems = 0;
+      let q = queue.borrow();
+      for i in 0 .. q.len() {
+        for j in 0 .. q.get_len(i) {
+          let size = q.get_size(i, j);
+          if nelems < n {
+            size_limit += size;
+            nelems += 1;
+          }
+        }
+      }
+
+      size_limit
+    }, |size_limit| {
+      let mut q = queue.borrow_mut();
+      let     r = q.split(std::i32::MAX, size_limit);
+      *q = r;
+    })
   }, &[1, 20, 100, 500]);
 }
 
-criterion_group!(benches, bench_insert, bench_split_half, bench_split_n);
+criterion_group!(benches, bench_insert_2000, bench_insert_7000, bench_insert_20000, bench_split_half, bench_split_n);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,98 @@
+extern crate rand;
+
 pub mod splitter {
   pub type Price = i32;
   pub type Size  = u32;
   pub type Meta  = u128;
 
-  pub type Queue = Vec<(Price, Vec<(Size, Meta)>)>;
+  #[derive(Clone, Debug, PartialEq)]
+  pub struct Queue(Vec<(Price, Vec<(Size, Meta)>)>);
 
-  /// Inserts given `Price`, `Size` and `Meta` into given `Queue` __in place__.
-  /// The `Queue` should be sorted by `Price` and stays sorted.
-  /// Worst-case `O(q.len())` because we shift elements when inserting.
-  /// Best-case `O(log(q.len()))`.
-  pub fn insert(q: &mut Queue, p: Price, s: Size, m: Meta) {
-    match q.binary_search_by_key(&p, |&(price, _)| price) {
-      Ok(ind) => {
-        // p is already there, just add another (s, m) to it
-        q[ind].1.push((s, m));
-      }
-      Err(ind) => {
-        // no such price was there, insert a fresh one in an `ind` position
-        q.insert(ind, (p, vec![(s, m)]));
-      }
+  impl Queue {
+    pub fn new(q: Vec<(Price, Vec<(Size, Meta)>)>) -> Queue {
+      Queue(q)
+    }
+
+    pub fn len(&self) -> usize {
+      self.0.len()
+    }
+
+    pub fn get_len(&self, i: usize) -> usize {
+      self.0[i].1.len()
+    }
+
+    pub fn get_size(&self, i: usize, j: usize) -> Size {
+      self.0[i].1[j].0
+    }
+
+    pub fn pop_rand(&mut self) {
+      use rand::prelude::*;
+      let mut rng = thread_rng();
+      let ind: usize = rng.gen_range(0, self.0.len());
+      self.0[ind].1.pop();
     }
   }
 
-  /// Splits given `Queue` according to given `Price` and `Size`.
-  /// `split` __does__ mutate `q`.
-  /// All the elements left in `q` are `<= p` and sum of all `Size`s is `<= s`.
-  /// All other elements are returned as a result.
-  /// A `(Price, Vec<(Size, Meta)>)` pair might be split between `q` and result.
-  /// Complexity is `O(q.len()*q.iter().map(|(_, v)| v.len()).max())`
-  /// if complexity of `split_off` is no more than `O(q.len())`.
-  pub fn split(q: &mut Queue, p: Price, s: Size) -> Queue {
-    let mut size_acc = 0;
+  pub trait Split {
+    fn insert(&mut self, p: Price, s: Size, m: Meta);
+    fn split(&mut self, p: Price, s: Size) -> Self;
+  }
 
-    let mindex = q.iter().position(|&(price, ref v)| {
-      size_acc += v.iter().map(|&(size, _)| size).sum::<u32>();
-      price > p || size_acc > s
-    });
+  impl Split for Queue {
 
-    if let Some(ind) = mindex {
-      // everything < ind is guaranteed to be under the limits
-      let mut res = q.split_off(ind);
-
-      // now what about res[0]? Should we split it?
-      let price = res[0].0;
-      if price <= p {
-        // we can split
-        size_acc -= res[0].1.iter().map(|&(size, _)| size).sum::<u32>();
-        let (addition, rest) = split_size(&res[0].1, size_acc, s);
-
-        q.push((price, addition));
-        res[0].1 = rest;
+    /// Inserts given `Price`, `Size` and `Meta` into given `Queue` __in place__.
+    /// The `Queue` should be sorted by `Price` and stays sorted.
+    /// Worst-case `O(q.len())` because we shift elements when inserting.
+    /// Best-case `O(log(q.len()))`.
+    fn insert(&mut self, p: Price, s: Size, m: Meta) {
+      match self.0.binary_search_by_key(&p, |&(price, _)| price) {
+        Ok(ind) => {
+          // p is already there, just add another (s, m) to it
+          self.0[ind].1.push((s, m));
+        }
+        Err(ind) => {
+          // no such price was there, insert a fresh one in an `ind` position
+          self.0.insert(ind, (p, vec![(s, m)]));
+        }
       }
+    }
 
-      res
-    } else {
-      // all elements are under the limits
-      // keep original vector as it is, return empty one
-      vec![]
+    /// Splits given `Queue` according to given `Price` and `Size`.
+    /// `split` __does__ mutate `q`.
+    /// All the elements left in `q` are `<= p` and sum of all `Size`s is `<= s`.
+    /// All other elements are returned as a result.
+    /// A `(Price, Vec<(Size, Meta)>)` pair might be split between `q` and result.
+    /// Complexity is `O(q.len()*q.iter().map(|(_, v)| v.len()).max())`
+    /// if complexity of `split_off` is no more than `O(q.len())`.
+    fn split(&mut self, p: Price, s: Size) -> Queue {
+      let mut size_acc = 0;
+
+      let mindex = self.0.iter().position(|&(price, ref v)| {
+        size_acc += v.iter().map(|&(size, _)| size).sum::<u32>();
+        price > p || size_acc > s
+      });
+
+      if let Some(ind) = mindex {
+        // everything < ind is guaranteed to be under the limits
+        let mut res = self.0.split_off(ind);
+
+        // now what about res[0]? Should we split it?
+        let price = res[0].0;
+        if price <= p {
+          // we can split
+          size_acc -= res[0].1.iter().map(|&(size, _)| size).sum::<u32>();
+          let (addition, rest) = split_size(&res[0].1, size_acc, s);
+
+          self.0.push((price, addition));
+          res[0].1 = rest;
+        }
+
+        Queue(res)
+      } else {
+        // all elements are under the limits
+        // keep original vector as it is, return empty one
+        Queue(vec![])
+      }
     }
   }
 
@@ -98,70 +134,70 @@ mod tests {
 
   #[test]
   fn split_3_15() {
-    let mut q: Queue = vec![(5, vec![(10, 2), (20, 3)]), (7, vec![(10, 40), (20, 50)])];
-    let rest = split(&mut q, 3, 15);
-    assert_eq!(rest, vec![(5, vec![(10, 2), (20, 3)]), (7, vec![(10, 40), (20, 50)])]);
-    assert_eq!(q, vec![]);
+    let mut q = Queue::new(vec![(5, vec![(10, 2), (20, 3)]), (7, vec![(10, 40), (20, 50)])]);
+    let rest  = q.split(3, 15);
+    assert_eq!(rest, Queue::new(vec![(5, vec![(10, 2), (20, 3)]), (7, vec![(10, 40), (20, 50)])]));
+    assert_eq!(q, Queue::new(vec![]));
   }
 
   #[test]
   fn split_6_15() {
-    let mut q: Queue = vec![(5, vec![(10, 2), (20, 3)]), (7, vec![(10, 40), (20, 50)])];
-    let rest = split(&mut q, 6, 15);
-    assert_eq!(rest, vec![(5, vec![(15, 3)]), (7, vec![(10, 40), (20, 50)])]);
-    assert_eq!(q, vec![(5, vec![(10, 2), (5, 3)])]);
+    let mut q = Queue::new(vec![(5, vec![(10, 2), (20, 3)]), (7, vec![(10, 40), (20, 50)])]);
+    let rest  = q.split(6, 15);
+    assert_eq!(rest, Queue::new(vec![(5, vec![(15, 3)]), (7, vec![(10, 40), (20, 50)])]));
+    assert_eq!(q, Queue::new(vec![(5, vec![(10, 2), (5, 3)])]));
   }
 
   #[test]
   fn split_8_15() {
-    let mut q: Queue = vec![(5, vec![(10, 2), (20, 3)]), (7, vec![(10, 40), (20, 50)])];
-    let rest = split(&mut q, 8, 15);
-    assert_eq!(rest, vec![(5, vec![(15, 3)]), (7, vec![(10, 40), (20, 50)])]);
-    assert_eq!(q, vec![(5, vec![(10, 2), (5, 3)])]);
+    let mut q = Queue::new(vec![(5, vec![(10, 2), (20, 3)]), (7, vec![(10, 40), (20, 50)])]);
+    let rest  = q.split(8, 15);
+    assert_eq!(rest, Queue::new(vec![(5, vec![(15, 3)]), (7, vec![(10, 40), (20, 50)])]));
+    assert_eq!(q, Queue::new(vec![(5, vec![(10, 2), (5, 3)])]));
   }
 
   #[test]
   fn split_8_35() {
-    let mut q: Queue = vec![(5, vec![(10, 2), (20, 3)]), (7, vec![(10, 40), (20, 50)])];
-    let rest = split(&mut q, 8, 35);
-    assert_eq!(rest, vec![(7, vec![(5, 40), (20, 50)])]);
-    assert_eq!(q, vec![(5, vec![(10, 2), (20, 3)]), (7, vec![(5, 40)])]);
+    let mut q = Queue::new(vec![(5, vec![(10, 2), (20, 3)]), (7, vec![(10, 40), (20, 50)])]);
+    let rest  = q.split(8, 35);
+    assert_eq!(rest, Queue::new(vec![(7, vec![(5, 40), (20, 50)])]));
+    assert_eq!(q, Queue::new(vec![(5, vec![(10, 2), (20, 3)]), (7, vec![(5, 40)])]));
   }
 
   #[test]
   fn split_6_100() {
-    let mut q: Queue = vec![(5, vec![(10, 2), (20, 3)]), (7, vec![(10, 40), (20, 50)])];
-    let rest = split(&mut q, 6, 100);
-    assert_eq!(rest, vec![(7, vec![(10, 40), (20, 50)])]);
-    assert_eq!(q, vec![(5, vec![(10, 2), (20, 3)])]);
+    let mut q = Queue::new(vec![(5, vec![(10, 2), (20, 3)]), (7, vec![(10, 40), (20, 50)])]);
+    let rest  = q.split(6, 100);
+    assert_eq!(rest, Queue::new(vec![(7, vec![(10, 40), (20, 50)])]));
+    assert_eq!(q, Queue::new(vec![(5, vec![(10, 2), (20, 3)])]));
   }
 
   #[test]
   fn split_100_100() {
-    let mut q: Queue = vec![(5, vec![(10, 2), (20, 3)]), (7, vec![(10, 40), (20, 50)])];
-    let rest = split(&mut q, 100, 100);
-    assert_eq!(rest, vec![]);
-    assert_eq!(q, vec![(5, vec![(10, 2), (20, 3)]), (7, vec![(10, 40), (20, 50)])]);
+    let mut q = Queue::new(vec![(5, vec![(10, 2), (20, 3)]), (7, vec![(10, 40), (20, 50)])]);
+    let rest  = q.split(100, 100);
+    assert_eq!(rest, Queue::new(vec![]));
+    assert_eq!(q, Queue::new(vec![(5, vec![(10, 2), (20, 3)]), (7, vec![(10, 40), (20, 50)])]));
   }
 
   #[test]
   fn insert_empty() {
-    let mut queue: Queue = vec![];
-    insert(&mut queue, 10, 2, 0);
-    assert_eq!(queue, vec![(10, vec![(2, 0)])]);
+    let mut queue = Queue::new(vec![]);
+    queue.insert(10, 2, 0);
+    assert_eq!(queue, Queue::new(vec![(10, vec![(2, 0)])]));
   }
 
   #[test]
   fn insert_new() {
-    let mut queue: Queue = vec![(10, vec![(2, 0)])];
-    insert(&mut queue, 20, 3, 20);
-    assert_eq!(queue, vec![(10, vec![(2, 0)]), (20, vec![(3, 20)])]);
+    let mut queue = Queue::new(vec![(10, vec![(2, 0)])]);
+    queue.insert(20, 3, 20);
+    assert_eq!(queue, Queue::new(vec![(10, vec![(2, 0)]), (20, vec![(3, 20)])]));
   }
 
   #[test]
   fn insert_existing() {
-    let mut queue: Queue = vec![(10, vec![(2, 0)])];
-    insert(&mut queue, 10, 3, 20);
-    assert_eq!(queue, vec![(10, vec![(2, 0), (3, 20)])]);
+    let mut queue = Queue::new(vec![(10, vec![(2, 0)])]);
+    queue.insert(10, 3, 20);
+    assert_eq!(queue, Queue::new(vec![(10, vec![(2, 0), (3, 20)])]));
   }
 }
